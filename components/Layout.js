3e5e9e25342dc442440ee2f8b719ece7d2b66aee
BayLadyCoder <rachada_32@hotmail.com>
2021-05-09 14:05:59 -0400

    Add Footer to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Header from "./Header";
+import Footer from "./Footer";
 import styles from "../styles/Layout.module.css";
 
 const Layout = ({ title, description, keywords, children }) => {
@@ -12,8 +13,8 @@ const Layout = ({ title, description, keywords, children }) => {
         <meta name="keywords" content={keywords} />
       </Head>
       <Header />
-      <div className={styles.container}></div>
-      {children}
+      <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 };
