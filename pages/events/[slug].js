b37eab3836092a42635897a52bd48ec9138e5344
BayLadyCoder <rachada_32@hotmail.com>
2021-05-09 13:06:30 -0400

    Added About page and add examples using nested routes and dynamic routes using folder events and [slug] file name

---
+++ pages/events/[slug].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DynamicPage = () => {
+  return (
+    <div>
+      <h1>Dynamic URL Page (Slug)</h1>
+    </div>
+  );
+};
+
+export default DynamicPage;
---
+++ pages/events/add.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddNewEvent = () => {
+  return (
+    <div>
+      <h1>Add New Event</h1>
+    </div>
+  );
+};
+
+export default AddNewEvent;
