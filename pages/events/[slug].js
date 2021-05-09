43f69ba1321cceb4c40d5e8b814e1f7fb179a6e4
BayLadyCoder <rachada_32@hotmail.com>
2021-05-09 13:18:57 -0400

    Use useRouter to get the slug from the URL of a dynamic route (router.query.slug)

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useRouter } from "next/router";
 
 const DynamicPage = () => {
+  const router = useRouter();
+  const slug = router.query.slug;
+  //   console.log(router);
   return (
     <div>
       <h1>Dynamic URL Page (Slug)</h1>
+      <h3>My slug: {slug}</h3>
     </div>
   );
 };
