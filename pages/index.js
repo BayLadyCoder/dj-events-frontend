ee87cef2c8f473e2bda481eb9a4c33183301cec7
BayLadyCoder <rachada_32@hotmail.com>
2021-05-17 22:26:59 -0400

    Add getStaticProp

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
 import Link from "next/link";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function Home() {
+export default function Home({ events }) {
+  console.log(events);
   return (
     <Layout>
-      <h1>Home</h1>
-      <Link href="/about">Link to About Page</Link>
+      <h1>Upcoming Events</h1>
     </Layout>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
+};
