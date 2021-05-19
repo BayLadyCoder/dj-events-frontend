0df34f475b859c896bd335a9af37c6fedc202c58
BayLadyCoder <rachada_32@hotmail.com>
2021-05-18 23:17:33 -0400

    Use getServerSideProps to get event detail from the backend

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,18 +1,30 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-const DynamicPage = () => {
+const EventPage = ({ events }) => {
   const router = useRouter();
   const slug = router.query.slug;
   //   console.log(router);
+  console.log("events", events);
   return (
     <Layout>
       <h1>Dynamic URL Page (Slug)</h1>
-      <h3>My slug: {slug}</h3>
+      <h3>{events.name}</h3>
       <button onClick={() => router.push("/")}>{"< "}Back to Homepage</button>
     </Layout>
   );
 };
 
-export default DynamicPage;
+export const getServerSideProps = async ({ query: { slug } }) => {
+  console.log(slug);
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: { events: events[0] },
+  };
+};
+
+export default EventPage;
